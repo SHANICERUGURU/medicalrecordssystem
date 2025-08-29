85e5c4ff3c1bba17486d42681b4db391de895ec9
SHANICERUGURU <rugurushanice990@gmail.com>
2025-08-29 08:08:21 +0300

    linked the registrstion form to the to the registration template and ensured smmoth flow from the register to login to dashboard.

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,12 +2,20 @@ from django import forms
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 class PatientForm(forms.ModelForm):
     class Meta:
         model=Patient
         fields=['first_name', 'last_name', 'date_of_birth','gender','phone','blood_type','allergies','medical_history','last_appointment','last_doctor','emergency_contact_name','emergency_contact_phone']
 
+User = get_user_model()       
 class RegisterForm(UserCreationForm):
     class Meta:
         model=User
-        fields=['username','email','password1','password2']        
+        fields=['username','email','password1','password2'] 
+
+        #    adding styling to the form created
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
--- myapp/views.py
+++ myapp/views.py
@@ -8,14 +8,14 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import *
-@api_view(['POST', 'GET'])
+
 def RegistrationView(request):
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
+            user.set_password(form.cleaned_data['password1'])
             user.save()
             messages.success(request, 'Registration successful. You can now log in.')
             return redirect('login')
@@ -30,7 +30,7 @@ def RegistrationView(request):
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password1']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
