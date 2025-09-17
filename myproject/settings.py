db0d9dae2e6cbbf0e8543fbac7076f826a672ba6
SHANICERUGURU <rugurushanice990@gmail.com>
2025-09-17 08:58:02 +0300

    setup my frontend

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') =='True'
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS =  ['localhost','127.0.0.1']
 
 REST_FRAMEWORK = { 
     "DEFAULT_AUTHENTICATION_CLASSES": ( 
